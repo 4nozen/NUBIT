08bfa2f892d71838cfed1ca5e86a3a6675d4479b
4nozen <4nozen@gmail.com>
2024-07-15 22:10:03 +0300

    reall! Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import os
 import subprocess
-import time
+
+
+welcome = """
+#######################################
+#                                     #
+#    Welcome NUBIT node manager...    #
+#                                     #
+#######################################
+"""
 
 file = 'running'
 path = os.path.join(os.getcwd(), file)
@@ -21,44 +29,51 @@ def start_node():
     os.system('clear')
     with open(path, 'r') as f:
         if f.read().strip() == 'false':
+            chars = ["-", "\\", "|", "/"]
+            index = 0
+            out_ = ''
             os.system('nohup bash <nubit.sh > $HOME/nubit-light.log 2>&1 &')
             is_running('true')
-            print("NODE STARTED RUNNING... the first start it takes time")
-            time.sleep(5)
+            print("NODE RUNNING... the first start it takes time")
+            while out_ == '':
+                char = chars[index % len(chars)]
+                print(f"Выполнение: [{char}] ", end='\r')
+                index += 1
+                out = subprocess.run("lsof -i :2121 | grep LISTEN", shell=True, capture_output=True, text=True)
+                out_ = out.stdout
+            print("NODE STARTED")
         else:
             print("Already running")
-    print("###########################")
     main()
 
 
 def status_node():
     os.system('clear')
-    os.system('lsof -i :2121 | grep LISTEN')
-    print("###########################")
+    out = subprocess.run("lsof -i :2121 | grep LISTEN", shell=True, capture_output=True, text=True)
+    print(out.stdout)
     main()
 
 def show_log_node():
     os.system('clear')
     os.system('tail -n 20 $HOME/nubit-light.log')
-    print("###########################")
     main()
 
 def further_check():
     os.system('clear')
     os.system('$HOME/nubit-node/bin/nubit das sampling-stats --node.store $HOME/.nubit-light-nubit-alphatestnet-1')
-    print("###########################")
     main()
 
 def show_pubkey():
     os.system('clear')
-    os.system('$HOME/nubit-node/bin/nkey list --p2p.network nubit-alphatestnet-1 --node.type light')
-    print("###########################")
+    # os.system('$HOME/nubit-node/bin/nkey list --p2p.network nubit-alphatestnet-1 --node.type light')
+    out = subprocess.run("$HOME/nubit-node/bin/nkey list --p2p.network nubit-alphatestnet-1 --node.type light", shell=True, capture_output=True, text=True)
+    print(out.stdout)
+
     main()
 
 def show_mnemonic():
     os.system('clear')
     os.system('cat $HOME/nubit-node/mnemonic.txt')
-    print("###########################")
     main()
 
 def delete_mnemonic():
@@ -76,14 +91,12 @@ def delete_mnemonic():
     else:
         print("Invalid choice. Please try again.")
         delete_mnemonic()
-    print("###########################")
 
 def stop_node():
     os.system('clear')
     os.system('kill $(lsof -t -i:2121)')
     is_running('false')
     print("NODE STOPED")
-    print("###########################")
     main()
 
 def exit_():
@@ -91,52 +104,38 @@ def exit_():
 
 def main():
     print("")
-    print("Welcome NUBIT node manager...")
-    print("")
-    print("###########################")
-    print("")
-    print("1. Start NUBIT node. !!the first start it takes time. wait a few minutes!! Node will start automatically in background mode.")
-    print("2. Status NUBIT node. Check whether the node is running or not. (wait a little after launch node)")
-    print("3. Show log NUBIT node. Check the latest log.")
-    print("")
-    print("4. For further check (Make sure your light node is running!)")
-    print("5. Show Nubit node ADDRESS and PUBKEY (Make sure your light node is running!)")
-    print("6. Show mnemonic words")
-    print("")
-    print("7. !!!Stop NUBIT node!!!")
-    print("8. !!!Delete mnemonic.txt!!!")
-    print("-")
-    print("0. Exit")
-    print("")
+    print(welcome)
+    print("""
+1. Start NUBIT node. Node will start automatically in background mode.
+2. Status NUBIT node. Check whether the node is running or not.
+3. Show log NUBIT node. Check the latest log.
+
+4. For further check. (Make sure your light node is running!)
+5. Show Nubit node ADDRESS and PUBKEY. (Make sure your light node is running!)
+6. Show mnemonic words.
+
+7. !!!Stop NUBIT node!!!
+8. !!!Delete mnemonic.txt!!!
+ 
+0. Exit
+""")
     
-    choice = int(input("Enter your choice: "))
-    if choice == 1:
-        start_node()
-    elif choice == 2:
-        status_node()
-    elif choice == 3:
-        show_log_node()
-    elif choice == 4:
-        further_check()
-    elif choice == 5:
-        show_pubkey()
-    elif choice == 6:
-        show_mnemonic()
-    elif choice == 7:
-        stop_node()
-    elif choice == 8:
-        delete_mnemonic()
-    elif choice == 0:
-        exit_()
-    else:
-        os.system('clear')
-        print("Invalid choice. Please try again.")
-        print("###########################")
-        main()
+    choice = input("Enter your choice: ")
+    if choice == "1": start_node()
+    elif choice == "2": status_node()
+    elif choice == "3": show_log_node()
+    elif choice == "4": further_check()
+    elif choice == "5": show_pubkey()
+    elif choice == "6": show_mnemonic()
+    elif choice == "7": stop_node()
+    elif choice == "8": delete_mnemonic()
+    elif choice == "0": exit_()
+    else: main()
 
 
 if __name__ == '__main__':
     os.system('clear')
+    # os.system('cd ~')
     os.system('curl https://nubit.sh > nubit.sh')
-
+    os.system('clear')
     main()
